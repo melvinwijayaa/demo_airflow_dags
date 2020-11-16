ae8a92fc042bb2ab678c32a207d586ce360c450f
Khairul Habib <mkhairulhabibm@gmail.com>
2020-11-16 18:06:50 +0700

    change function to external all!

--- lithops_function.py
+++ lithops_function.py
@@ -1,3 +1,26 @@
+import lithops
+
+# Dataset from: https://archive.ics.uci.edu/ml/datasets/bag+of+words
+iterdata = ['https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.enron.txt',
+            'https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.kos.txt',
+            'https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.nips.txt',
+            'https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.nytimes.txt',
+            'https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.pubmed.txt']
+
+config = {'lithops' : {'storage_bucket' : 'lithops-bucket-habib01',
+                        'storage':'ibm_cos',
+                        'mode':'serverless'},
+          'serverless':{'backend':'ibm_cf'},
+          'ibm':{'iam_api_key':'cLQhHWR28nlJaGOqo7j87L5akzoCizqQPvH_XooHHo3h'},
+
+          'ibm_cf':  {'endpoint': 'https://us-south.functions.cloud.ibm.com',
+                      'namespace': 'Namespace-H5L',
+                      'namespace_id': '7fd17f8c-4a89-4d08-9529-f9aa7737c52d'},
+
+          'ibm_cos': {'endpoint': 'https://s3.jp-tok.cloud-object-storage.appdomain.cloud',
+                      'private_endpoint': 'https://s3.private.jp-tok.cloud-object-storage.appdomain.cloud',
+                      'api_key': 'jlFa8a1ERFLryXzLhVT4Z0HbYaUdwW_UsGCLDPlaCnm2'}}
+
 def my_map_function(url):
     print('I am processing the object from {}'.format(url.path))
     counter = {}
@@ -24,3 +47,7 @@ def my_reduce_function(results):
                 final_result[word] += count[word]
 
     return final_result
+
+def lithops_func(*op_args):
+    fexec = lithops.FunctionExecutor(config=config)
+    fexec.map_reduce(my_map_function, iterdata, my_reduce_function)
--- lithops_word_processing.py
+++ lithops_word_processing.py
@@ -3,20 +3,15 @@ from builtins import range
 from airflow.operators.python_operator import PythonOperator
 from airflow.models import DAG
 from datetime import datetime, timedelta
-import lithops
 
 import time
 from pprint import pprint
 
 from lithops_function import my_map_function
 from lithops_function import my_reduce_function
+from lithops_function import lithops_func
 
-# Dataset from: https://archive.ics.uci.edu/ml/datasets/bag+of+words
-iterdata = ['https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.enron.txt',
-            'https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.kos.txt',
-            'https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.nips.txt',
-            'https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.nytimes.txt',
-            'https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.pubmed.txt']
+
 
 seven_days_ago = datetime.combine(
         datetime.today() - timedelta(7), datetime.min.time())
@@ -26,27 +21,12 @@ args = {
     'start_date': seven_days_ago,
 }
 
-config = {'lithops' : {'storage_bucket' : 'lithops-bucket-habib01',
-                        'storage':'ibm_cos',
-                        'mode':'serverless'},
-          'serverless':{'backend':'ibm_cf'},
-          'ibm':{'iam_api_key':'cLQhHWR28nlJaGOqo7j87L5akzoCizqQPvH_XooHHo3h'},
-
-          'ibm_cf':  {'endpoint': 'https://us-south.functions.cloud.ibm.com',
-                      'namespace': 'Namespace-H5L',
-                      'namespace_id': '7fd17f8c-4a89-4d08-9529-f9aa7737c52d'},
-
-          'ibm_cos': {'endpoint': 'https://s3.jp-tok.cloud-object-storage.appdomain.cloud',
-                      'private_endpoint': 'https://s3.private.jp-tok.cloud-object-storage.appdomain.cloud',
-                      'api_key': 'jlFa8a1ERFLryXzLhVT4Z0HbYaUdwW_UsGCLDPlaCnm2'}}
 
 dag = DAG(
     dag_id='lithops_word_processing', default_args=args,
     schedule_interval=None)
 
-def lithops_func(*op_args):
-    fexec = lithops.FunctionExecutor()
-    fexec.map_reduce(my_map_function, iterdata, my_reduce_function)
+
 
 run_this = PythonOperator(
     task_id='word_processing',
