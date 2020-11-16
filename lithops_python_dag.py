2170122c813b20d489201554609286a91c389a2e
Khairul Habib <mkhairulhabibm@gmail.com>
2020-11-16 17:03:40 +0700

    add lithops func fix name

--- lithops_python_dag.py
+++ lithops_python_dag.py
@@ -17,7 +17,7 @@ args = {
 }
 
 dag = DAG(
-    dag_id='example_python_operator', default_args=args,
+    dag_id='lithops_python_operator', default_args=args,
     schedule_interval=None)
 
 def hello_world(name):
