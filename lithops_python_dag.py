a59cc79ca5667fd172d7592155864fe60009e1da
Khairul Habib <mkhairulhabibm@gmail.com>
2020-11-16 17:00:03 +0700

    add lithops func

---
+++ lithops_python_dag.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+from builtins import range
+from airflow.operators import PythonOperator
+from airflow.models import DAG
+from datetime import datetime, timedelta
+import lithops
+
+import time
+from pprint import pprint
+
+seven_days_ago = datetime.combine(
+        datetime.today() - timedelta(7), datetime.min.time())
+
+args = {
+    'owner': 'airflow',
+    'start_date': seven_days_ago,
+}
+
+dag = DAG(
+    dag_id='example_python_operator', default_args=args,
+    schedule_interval=None)
+
+def hello_world(name):
+    return 'Hello {}!'.format(name)
+
+def lithops_func(*op_args):
+    fexec = lithops.FunctionExecutor()
+    fexec.call_async(hello_world, op_args[0])
+    print(fexec.get_result())
+
+run_this = PythonOperator(
+    task_id='print_the_context',
+    provide_context=True,
+    python_callable=lithops_func,
+    op_args=['World', 'Earth', 'Forest'],
+    dag=dag)
+
+run_that = PythonOperator(
+    task_id='print_the_second_context',
+    provide_context=True,
+    python_callable=lithops_func,
+    op_args=['Earth', 'World', 'Forest'],
+    dag=dag)
+
+run_this >> run_that
