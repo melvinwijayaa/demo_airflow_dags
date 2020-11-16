d8762f439a760d83fb011a8adbf7144d3e234a2b
Khairul Habib <mkhairulhabibm@gmail.com>
2020-11-16 17:08:01 +0700

    add config lithops

--- lithops_python_dag.py
+++ lithops_python_dag.py
@@ -16,6 +16,20 @@ args = {
     'start_date': seven_days_ago,
 }
 
+config = {'lithops' : {'storage_bucket' : 'lithops-bucket-habib01',
+                        'storage':'ibm_cos',
+                        'mode':'serverless'},
+          'serverless':{'backend':'ibm_cf'},
+          'ibm':{'iam_api_key':'cLQhHWR28nlJaGOqo7j87L5akzoCizqQPvH_XooHHo3h'},
+
+          'ibm_cf':  {'endpoint': 'https://us-south.functions.cloud.ibm.com',
+                      'namespace': 'Namespace-H5L',
+                      'namespace_id': '7fd17f8c-4a89-4d08-9529-f9aa7737c52d'},
+
+          'ibm_cos': {'endpoint': 'https://s3.jp-tok.cloud-object-storage.appdomain.cloud',
+                      'private_endpoint': 'https://s3.private.jp-tok.cloud-object-storage.appdomain.cloud',
+                      'api_key': 'jlFa8a1ERFLryXzLhVT4Z0HbYaUdwW_UsGCLDPlaCnm2'}}
+
 dag = DAG(
     dag_id='lithops_python_operator', default_args=args,
     schedule_interval=None)
@@ -23,23 +37,21 @@ dag = DAG(
 def hello_world(name):
     return 'Hello {}!'.format(name)
 
-def lithops_func(*op_args):
-    fexec = lithops.FunctionExecutor()
-    fexec.call_async(hello_world, op_args[0])
+def lithops_func():
+    fexec = lithops.FunctionExecutor(conf=config)
+    fexec.call_async(hello_world, 'World')
     print(fexec.get_result())
 
 run_this = PythonOperator(
     task_id='print_the_context',
     provide_context=True,
     python_callable=lithops_func,
-    op_args=['World', 'Earth', 'Forest'],
     dag=dag)
 
 run_that = PythonOperator(
     task_id='print_the_second_context',
     provide_context=True,
     python_callable=lithops_func,
-    op_args=['Earth', 'World', 'Forest'],
     dag=dag)
 
 run_this >> run_that
