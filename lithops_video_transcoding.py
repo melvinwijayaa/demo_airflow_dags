98f071b325d88e2936b623275baef04ee7645567
Muhammad Khairul Habib Muttaqien <khairul.habib@ibm.com>
2020-11-23 09:18:03 +0700

    add video transcoder

---
+++ lithops_video_transcoding.py
@@ -0,0 +1,33 @@
+import lithops
+import ffmpeg
+from storage_util import upload_file
+
+config = {'lithops' : {'storage_bucket' : 'lithops-bucket-habib01',
+                        'storage':'ibm_cos',
+                        'mode':'serverless'},
+          'serverless':{'backend':'ibm_cf'},
+          'ibm':{'iam_api_key':'cLQhHWR28nlJaGOqo7j87L5akzoCizqQPvH_XooHHo3h'},
+
+          'ibm_cf':  {'endpoint': 'https://us-south.functions.cloud.ibm.com',
+                      'namespace': 'Namespace-H5L',
+                      'namespace_id': '7fd17f8c-4a89-4d08-9529-f9aa7737c52d'},
+
+          'ibm_cos': {'endpoint': 'https://s3.jp-tok.cloud-object-storage.appdomain.cloud',
+                      'private_endpoint': 'https://s3.private.jp-tok.cloud-object-storage.appdomain.cloud',
+                      'api_key': 'jlFa8a1ERFLryXzLhVT4Z0HbYaUdwW_UsGCLDPlaCnm2'}}
+
+def process_video(input_url):
+    input = ffmpeg.input(input_url)
+    audio = input.audio.filter("aecho", 0.8, 0.9, 1000, 0.3)
+    video = input.video.hflip()
+    out = ffmpeg.output(audio, video, 'output.mp4')
+    ffmpeg.run(out)
+    f = open('output.mp4','rb')
+    upload_file("test-bucket-fn",'ouput-lithops.mp4',f)
+    print('finish uploading')
+    f.close()
+
+def run(*op_args):
+    fexec = lithops.FunctionExecutor()
+    fexec.call_async(process_video,op_args[0])
+    print(fexec.get_result())
--- video_dag.py
+++ video_dag.py
@@ -3,31 +3,32 @@ from builtins import range
 from airflow.operators.python_operator import PythonOperator
 from airflow.models import DAG
 from datetime import datetime, timedelta
+from lithops_video_transcoding import run
 
 import time
 from pprint import pprint
 
 from lithops_function import lithops_func
 
-seven_days_ago = datetime.combine(
-        datetime.today() - timedelta(7), datetime.min.time())
+yesterday = datetime.combine(
+        datetime.today() - timedelta(1), datetime.min.time())
 
 args = {
     'owner': 'airflow',
-    'start_date': seven_days_ago,
+    'start_date': yesterday,
 }
 
 
 dag = DAG(
-    dag_id='lithops_word_processing', default_args=args,
+    dag_id='video_dag', default_args=args,
     schedule_interval=None)
 
 
 
 run_this = PythonOperator(
-    task_id='word_processing',
-    python_callable=lithops_func,
-    op_args=['World'],
+    task_id='video_dag',
+    python_callable=run,
+    op_args=['http://techslides.com/demos/sample-videos/small.mp4'],
     dag=dag)
 
 run_this
