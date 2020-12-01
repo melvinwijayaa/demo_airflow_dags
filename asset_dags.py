914ab44bad490d394a8945f7abee13f0ef600819
gnugroho <66710662+gnugroho@users.noreply.github.com>
2020-12-01 20:52:02 +0700

    Update asset_dags.py

--- asset_dags.py
+++ asset_dags.py
@@ -1,38 +1,31 @@
 # airflow related
-from airflow import models
-from airflow import DAG
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
 
 # other packages
 from datetime import datetime, timedelta
 
 # import operators from the 'operators' file
-from asset_inventory import run
-from asset_inventory_deployment import run
+from asset_inventory import connect
+from asset_inventory_deployment import connect
 
 default_args = {
     "owner": "airflow",
-    "depends_on_past": False,
-    "start_date": datetime(2020, 8, 13),
-    "email": ["airflow@airflow.com"],
-    "email_on_failure": False,
-    "email_on_retry": False,
-    "retries": 1,
-    "retry_delay": timedelta(minutes=5),
-    "catchup": False,
+    "start_date": datetime(2020, 12, 1),
 }
 
-dag = DAG("asset_dag", default_args=default_args, schedule_interval=timedelta(1))
+dag = DAG("asset_dag", default_args=default_args, schedule_interval=None)
 
 asset_inventory = PythonOperator(
     task_id='asset_inventory',
-    python_callable=run,
-    op_args=['asset_inventory'],
+    python_callable=connect,
+    tablename=['asset_inventory'],
     dag=dag)
 
 asset_inventory_deployment = PythonOperator(
     task_id='asset_inventory_deployment',
-    python_callable=run,
-    op_args=['asset_inventory_deployment'],
+    python_callable=connect,
+    tablename=['asset_inventory_deployment'],
     dag=dag)
 
 asset_inventory >> asset_inventory_deployment
