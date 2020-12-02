877945cbaaa6f87ccc6b87754c080085c5366965
gnugroho <66710662+gnugroho@users.noreply.github.com>
2020-12-02 14:10:06 +0700

    Update asset_inventory_deployment.py

--- asset_inventory_deployment.py
+++ asset_inventory_deployment.py
@@ -42,7 +42,7 @@ def asset_inventory_deployment(tablename):
     print(cur2.rowcount, "Record inserted successfully into " +tablename)
 
 
-#if __name__ == '__main__':
+if __name__ == '__main__':
     fexec = lithops.FunctionExecutor()
     fexec.call_async(asset_inventory_deployment,'asset_inventory_deployment')
     print(fexec.get_result())
