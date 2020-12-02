8ba8bfc5dcccd8ebe6f4d9b29ee0832883f1c1ef
gnugroho <66710662+gnugroho@users.noreply.github.com>
2020-12-02 18:43:45 +0700

    Update jti_datalake_dags.py

--- jti_datalake_dags.py
+++ jti_datalake_dags.py
@@ -1,6 +1,7 @@
 # airflow related
 from airflow.models import DAG
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
 
 # other packages
 from datetime import datetime, timedelta
