b98a0d151b300f3df91bd6490832d9599babac92
gnugroho <66710662+gnugroho@users.noreply.github.com>
2021-01-15 10:12:01 +0700

    Add files via upload

--- glo_city_prod_dag.py
+++ glo_city_prod_dag.py
@@ -1,102 +1,81 @@
-# airflow related
-from airflow.models import DAG
-from airflow.operators.python_operator import PythonOperator
-from airflow.operators.dummy_operator import DummyOperator
 
-# other packages
-from datetime import datetime, timedelta
+import pyodbc
+import psycopg2
+import lithops
 
-# import operators from the 'operators' file
-from glo_city_prod import glo_city_prod_function
-
-# import lithops function
-#from jti_lithops_function import jti_lithops_function 
-
-default_args = {
-    "owner": "airflow",
-    "start_date": datetime(2021, 1, 14),
-    "retries": 1,
-    "retry_delay": timedelta(minutes=5),
+config = {
+'lithops': {
+    'storage': 'ibm_cos',
+    'storage_bucket': 'bucket-jti',
+    'mode': 'serverless'
+    },
+'serverless':{
+    'backend': 'ibm_cf',
+    'runtime': 'khairulhabib/lithops-runtime-datalake:1.0.1'
+    },
+'ibm':{
+    'iam_api_key': 'L8cWWexAcTm8K-XtlthzkwtNxZjxQwtnCcwr4Gj0qhkg'
+    },
+'ibm_cf':{
+    'endpoint'     : 'https://jp-tok.functions.cloud.ibm.com',
+    'namespace'    : 'JTI Dev',
+    'namespace_id' : 'f02917fc-e645-4850-9dea-5f27b541b933'
+    },
+'ibm_cos':{
+    'endpoint'    : 'https://s3.au-syd.cloud-object-storage.appdomain.cloud',
+    'private_endpoint': 'https://s3.private.au-syd.cloud-object-storage.appdomain.cloud',
+    'api_key '    : 'p6D4IagLwAXetJCWAzlsyzPnWezeIPicE-2j2LggG7HO'
+    #'access_key' : <ACCESS_KEY>  # Optional
+    #'secret_key' : <SECRET_KEY>  # Optional
+    },
 }
+def glo_city_prod_function(tablename):
+    fexec = lithops.FunctionExecutor()
+    fexec.call_async(glo_city,tablename)
+    print(fexec.get_result())    
+    
+def glo_city_prod(tablename):
 
-dag = DAG("asset_test_dag", default_args=default_args, schedule_interval=None)
+    #Fixed conexion string for connecting sqlserver -- no need to change 
+    conn1 = pyodbc.connect(
+        'DRIVER={PostgreSQL Unicode};'
+        'Server=c0ca2771-62ed-4c2a-862e-743fda10b364.bqfh4fpt0vhjh7rs4ot0.databases.appdomain.cloud;'
+        'Port=32645;'
+        'Database=ibmclouddb;'
+        'User ID=ibm_cloud_f261f536_a6f2_4fec_b8e9_55016c16b459;'
+        'Password=a4285df0d0f18926f9c84591c78f91d402ab3d037e8ef6023f0fb4ff41e45043';
+        'String Types=Unicode')
 
-#DummyOperator DAGS here
-staging_start = DummyOperator(
-    task_id='Staging_Start',
-    dag=dag)
+    #Fixed conexion string for connecting postgresql -- no need to change     
+    conn2 = psycopg2.connect(
+        database = 'ibmclouddb' ,
+        user = 'ibm_cloud_09847fd3_051e_4a89_b3d4_5ad2834bd12a' ,
+        password = 'b3031a57615d6aa121981db35ea33d0e07e21bfb34af76e255b1e5236ae14f13',
+        host = '0541b373-b134-406f-9687-f94b85d5cb94.bqfh4fpt0vhjh7rs4ot0.databases.appdomain.cloud',
+        port = '30280')
+        
+    #Retrieve data -- change here
+    cur1 = conn1.cursor()
+    cur1.execute("SELECT id, stat, createdby, createddate, createdip, updatedby, updateddate, updatedip, name FROM "+ tablename)
+    records = cur1.fetchall()
+    #conn1.commit() -- no need to commit
 
-staging_done = DummyOperator(
-    task_id='Staging_Done',
-    dag=dag)
+    #Delete data --change here
+    cur2 = conn2.cursor()
+    cur2.execute("DELETE FROM dimension." +tablename)
+    conn2.commit()
 
-datalake_start = DummyOperator(
-    task_id='Datalake_Start',
-    dag=dag)
+    print(cur2.rowcount, "Records deleted successfully from " +tablename)
 
-datalake_done = DummyOperator(
-    task_id='Datalake_Done',
-    dag=dag)
+    #Insert data -- change here
+    cur2 = conn2.cursor()
+    cur2.executemany("INSERT INTO dimension." +tablename+ "(id, stat, createdby, createddate, createdip, updatedby, updateddate, updatedip, name) \
+        VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)",records)
+    conn2.commit()
 
-dimension_start = DummyOperator(
-    task_id='Dimension_Start',
-    dag=dag)
-
-dimension_done = DummyOperator(
-    task_id='Dimension_Done',
-    dag=dag)
-
-fact_start = DummyOperator(
-    task_id='Fact_Start',
-    dag=dag)
-
-fact_done = DummyOperator(
-    task_id='Fact_Done',
-    dag=dag)
-
-datamart_start = DummyOperator(
-    task_id='DataMart_Start',
-    dag=dag)
-
-datamart_done = DummyOperator(
-    task_id='DataMart_Done',
-    dag=dag)
-
-olap_start = DummyOperator(
-    task_id='OLAP_Start',
-    dag=dag)
-
-olap_done = DummyOperator(
-    task_id='OLAP_Done',
-    dag=dag)
-
-#Add database staging here ...
-jtiiasset = DummyOperator(
-    task_id='jtiiasset',
-    dag=dag)
-
-livejtiipdbms = DummyOperator(
-    task_id='livejtiipdbms',
-    dag=dag)
-
-jtiifinace = DummyOperator(
-    task_id='jtiifinace',
-    dag=dag)
-
-livejtiipayroll = DummyOperator(
-    task_id='livejtiipayroll',
-    dag=dag)
-
-#Add more database staging here ...
-
-
-#PythonOperator extract staging tables
-glo_city_prod = PythonOperator(
-    task_id='glo_city_prod',
-    python_callable=glo_city_prod_function,
-    op_args=['glo_city_prod'],
-    dag=dag)
-
-
-#DAG Sequences
-staging_start >> glo_city_prod >> staging_done
+    print(cur2.rowcount, "Record inserted successfully into " +tablename)
+    
+if __name__ == '__main__':
+    fexec = lithops.FunctionExecutor()
+    fexec.call_async(glo_city_prod,'glo_city_prod')
+    print(fexec.get_result())
