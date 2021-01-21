6fc742fd8a08661ea594207d8f803cec9f3982b4
gnugroho <66710662+gnugroho@users.noreply.github.com>
2021-01-21 10:45:14 +0700

    Add files via upload

--- jtiiasset_test_dag.py
+++ jtiiasset_test_dag.py
@@ -132,13 +132,13 @@ asset_inventory_deployment = PythonOperator(
 asset_inventory_prod = PythonOperator(
     task_id='asset_inventory_prod',
     python_callable=asset_inventory_prod_function,
-    op_args=['asset_inventory_prod'],
+    op_args=['asset_inventory'],
     dag=dag)
 
 asset_inventory_deployment_prod_ = PythonOperator(
     task_id='asset_inventory_deployment_prod',
     python_callable=asset_inventory_deployment_prod_function,
-    op_args=['asset_inventory_deployment_prod'],
+    op_args=['asset_inventory_deployment'],
     dag=dag)
 
 
