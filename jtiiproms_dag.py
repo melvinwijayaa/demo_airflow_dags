174cebe8ea58392207ff66e1cd51197e6933fe8e
melvinwijayaa <77770944+melvinwijayaa@users.noreply.github.com>
2021-02-19 16:26:40 +0700

    Create jtiiproms_dag.py

---
+++ jtiiproms_dag.py
@@ -0,0 +1,94 @@
+# airflow related
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+
+# other packages
+from datetime import datetime, timedelta
+
+# import operators from the 'operators' file
+from contract_termination_request import contract_termination_request_function
+from glo_approvalrecord import glo_approvalrecord_function
+from glo_customer import glo_customer_function
+from glo_location import glo_location_function
+from num_department import num_department_function
+from proj_contractreg import proj_contractreg_function
+from proj_mainsetting import proj_mainsetting_function
+from proj_sowcategory import proj_sowcategory_function
+from proj_sowlayout import proj_sowlayout_function
+from proj_warmbodyreg import proj_warmbodyreg_function
+
+
+# import lithops function
+#from jti_lithops_function import jti_lithops_function 
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2021, 1, 19),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+dag = DAG("jtiiproms_dag", default_args=default_args, schedule_interval=None)
+
+contract_termination_request = PythonOperator(
+    task_id='contract_termination_request',
+    python_callable=contract_termination_request_function,
+    op_args=['contract_termination_request'],
+    dag=dag)
+
+glo_approvalrecord = PythonOperator(
+    task_id='glo_approvalrecord',
+    python_callable=glo_approvalrecord_function,
+    op_args=['glo_approvalrecord'],
+    dag=dag)
+
+glo_customer = PythonOperator(
+    task_id='glo_customer',
+    python_callable=glo_customer_function,
+    op_args=['glo_customer'],
+    dag=dag)
+
+glo_location = PythonOperator(
+    task_id='glo_location',
+    python_callable=glo_location_function,
+    op_args=['glo_location'],
+    dag=dag)
+
+num_department = PythonOperator(
+    task_id='num_department',
+    python_callable=num_department_function,
+    op_args=['num_department'],
+    dag=dag)
+
+proj_contractreg = PythonOperator(
+    task_id='proj_contractreg',
+    python_callable=proj_contractreg_function,
+    op_args=['proj_contractreg'],
+    dag=dag)
+
+proj_mainsetting = PythonOperator(
+    task_id='proj_mainsetting',
+    python_callable=proj_mainsetting_function,
+    op_args=['proj_mainsetting'],
+    dag=dag)
+
+proj_sowcategory = PythonOperator(
+    task_id='proj_sowcategory',
+    python_callable=proj_sowcategory_function,
+    op_args=['proj_sowcategory'],
+    dag=dag)
+
+proj_sowlayout = PythonOperator(
+    task_id='proj_sowlayout',
+    python_callable=proj_sowlayout_function,
+    op_args=['proj_sowlayout'],
+    dag=dag)
+
+proj_warmbodyreg = PythonOperator(
+    task_id='proj_warmbodyreg',
+    python_callable=proj_warmbodyreg_function,
+    op_args=['proj_warmbodyreg'],
+    dag=dag)
+
+#DAG Sequences
+contract_termination_request >> glo_approvalrecord >> glo_customer >> glo_location >> num_department >> proj_contractreg >> proj_mainsetting >> proj_sowcategory >> proj_sowlayout >> proj_warmbodyreg
